516c53135305e0c382dc1313f9c178fc6d856fe3
Billy Kid Andersson <billy.andersson@student.htw-berlin.de>
2024-01-10 11:15:02 +0100

    privacy flip switch added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,55 @@
-import MenuBar from "./MenuBar";
-import MusicMode from "./MusikMode";
-import StyleMode from "./StyleMode";
-import PrivateMode from "./PrivateMode";
-import React from "react";
-import StateMode from "./StateMode";
-function App() {
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import MenuBar from './MenuBar';
+import MusicMode from './MusikMode';
+import StyleMode from './StyleMode';
+import PrivateMode from './PrivateMode';
+import StateMode from './StateMode';
 
-    let destination ;
-    switch (window.location.pathname){
-        case "/Music": destination = <MusicMode/>
-            break
-        case "/Style": destination = <StyleMode/>
-            break
-        case "/Privacy": destination = <PrivateMode/>
-            break
-        case "/State": destination = <StateMode/>
-            break
+function App() {
+    const [privateMode, setPrivateMode] = useState(false);
+
+    let destination;
+    switch (window.location.pathname) {
+        case '/Music':
+            destination = <MusicMode />;
+            break;
+        case '/Style':
+            destination = <StyleMode />;
+            break;
+        case '/Privacy':
+            destination = <PrivateMode />;
+            break;
+        case '/State':
+            destination = <StateMode />;
+            break;
+        default:
+            destination = null;
+            break;
     }
-  return (
-    <div className="App">
-       <MenuBar/>
-        {destination}
-    </div>
-  );
+
+    const togglePrivateMode = () => {
+        setPrivateMode(!privateMode);
+    };
+
+    return (
+        <div className="App">
+            <MenuBar />
+            <div className="flip-switch">
+                <div className={`switch ${privateMode ? 'active' : ''}`} onClick={togglePrivateMode}>
+                    <div className="track">
+                        <div className="knob" />
+                    </div>
+                </div>
+                <span className="icon">
+                    <FontAwesomeIcon icon={privateMode ? faEyeSlash : faEye} />
+                </span>
+            </div>
+            <span className="toggle-label"></span>
+            {destination}
+        </div>
+    );
 }
+
 export default App;
--- src/MenuBar.js
+++ src/MenuBar.js
@@ -1,47 +1,54 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPalette, faMusic, faUserSecret, faChartBar } from '@fortawesome/free-solid-svg-icons'; // Import the specific icons you want to use
+import { faPalette, faMusic, faUserSecret, faChartBar, faQuestion } from '@fortawesome/free-solid-svg-icons';
 
 import './App.css'; // Import your CSS file
 
 const App = () => {
+    const handleNavigation = (route) => {
+        window.location.href = route;
+    };
+
     return (
         <nav className="navbar">
             <ul>
-                <li>
-                    <button className="nav-btn" onClick={() => window.location.href = "/Music"}>
+                <li onClick={() => handleNavigation('/Music')}>
+                    <div className="nav-item">
                         <FontAwesomeIcon icon={faMusic} size="2x" />
-                        <span>Music</span>
-                    </button>
+                    </div>
+                    <span>Music</span>
                 </li>
-                <li>
-                    <button className="nav-btn" onClick={() => window.location.href = "/State"}>
+                <li onClick={() => handleNavigation('/State')}>
+                    <div className="nav-item">
                         <FontAwesomeIcon icon={faChartBar} size="2x" />
-                        <span>Status</span>
-                    </button>
+                    </div>
+                    <span>Status</span>
                 </li>
-                <li>
-                    <button className="nav-btn" onClick={() => window.location.href = "/Style"}>
+                <li onClick={() => handleNavigation('/Style')}>
+                    <div className="nav-item">
                         <FontAwesomeIcon icon={faPalette} size="2x" />
-                        <span>Style</span>
-                    </button>
+                    </div>
+                    <span>Style</span>
                 </li>
-                <li>
-                    <button className="nav-btn" onClick={() => window.location.href = "/Music"}>
-                        <FontAwesomeIcon icon={faMusic} size="2x" />
-                        <span>Music</span>
-                    </button>
+                <li onClick={() => handleNavigation('/Music')}>
+                    <div className="nav-item">
+                        <div className="icon-container">
+                            <FontAwesomeIcon icon={faMusic} size="2x" />
+                            <FontAwesomeIcon icon={faQuestion} size="2x" className="question-mark" />
+                        </div>
+                    </div>
+                    <span>MystShare</span>
                 </li>
-                <li>
-                    <button className="nav-btn" onClick={() => window.location.href = "/Privacy"}>
+                <li onClick={() => handleNavigation('/Privacy')}>
+                    <div className="nav-item">
                         <FontAwesomeIcon icon={faUserSecret} size="2x" />
-                        <span>DND</span>
-                    </button>
+                    </div>
+                    <span>DND</span>
                 </li>
-
             </ul>
         </nav>
     );
 };
 
 export default App;
+
