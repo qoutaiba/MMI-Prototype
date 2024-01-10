9381ae6feec5de60dfdb2416682c7c46b3657dc4
Billy Kid Andersson <billy.andersson@student.htw-berlin.de>
2024-01-10 09:56:36 +0100

    änderung home screen

--- src/MenuBar.js
+++ src/MenuBar.js
@@ -1,22 +1,46 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPalette, faMusic, faUserSecret, faChartBar } from '@fortawesome/free-solid-svg-icons'; // Import the specific icons you want to use
+
+import './App.css'; // Import your CSS file
+
 const App = () => {
     return (
-       <nav>
-           <ul>
-               <li>
-                   <a href="Style"> Style mode</a>
-               </li>
-               <li>
-                   <a href="Music"> Music mode</a>
-               </li>
-               <li>
-                   <a href="Privacy"> Change to ghost mode</a>
-               </li>
-               <li>
-                   <a href="State"> State mode</a>
-               </li>
-           </ul>
-       </nav>
+        <nav className="navbar">
+            <ul>
+                <li>
+                    <button className="nav-btn" onClick={() => window.location.href = "/Music"}>
+                        <FontAwesomeIcon icon={faMusic} size="2x" />
+                        <span>Music</span>
+                    </button>
+                </li>
+                <li>
+                    <button className="nav-btn" onClick={() => window.location.href = "/State"}>
+                        <FontAwesomeIcon icon={faChartBar} size="2x" />
+                        <span>Status</span>
+                    </button>
+                </li>
+                <li>
+                    <button className="nav-btn" onClick={() => window.location.href = "/Style"}>
+                        <FontAwesomeIcon icon={faPalette} size="2x" />
+                        <span>Style</span>
+                    </button>
+                </li>
+                <li>
+                    <button className="nav-btn" onClick={() => window.location.href = "/Music"}>
+                        <FontAwesomeIcon icon={faMusic} size="2x" />
+                        <span>Music</span>
+                    </button>
+                </li>
+                <li>
+                    <button className="nav-btn" onClick={() => window.location.href = "/Privacy"}>
+                        <FontAwesomeIcon icon={faUserSecret} size="2x" />
+                        <span>DND</span>
+                    </button>
+                </li>
+
+            </ul>
+        </nav>
     );
 };
 
