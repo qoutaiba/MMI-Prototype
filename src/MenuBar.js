6bf022ae6454d893f50564e84f5cc7bc533781b9
Billy Kid Andersson <billy.andersson@student.htw-berlin.de>
2024-01-16 14:10:19 +0100

    BUTTOON LEUCHTET JEZTE MIT FARBE

--- src/MenuBar.js
+++ src/MenuBar.js
@@ -5,6 +5,7 @@ import { faPalette, faMusic, faUserSecret, faChartBar, faQuestion } from '@forta
 import './App.css'; // Import your CSS file
 
 const App = () => {
+    const currentPath = window.location.pathname;
     const handleNavigation = (route) => {
         window.location.href = route;
     };
@@ -12,25 +13,25 @@ const App = () => {
     return (
         <nav className="navbar">
             <ul>
-                <li onClick={() => handleNavigation('/Music')}>
+                <li className={currentPath === '/Music' ? 'selected' : ''} onClick={() => handleNavigation('/Music')}>
                     <div className="nav-item">
                         <FontAwesomeIcon icon={faMusic} size="2x" />
                     </div>
                     <span>Music</span>
                 </li>
-                <li onClick={() => handleNavigation('/State')}>
+                <li className={currentPath === '/State' ? 'selected' : ''} onClick={() => handleNavigation('/State')}>
                     <div className="nav-item">
                         <FontAwesomeIcon icon={faChartBar} size="2x" />
                     </div>
                     <span>Status</span>
                 </li>
-                <li onClick={() => handleNavigation('/Style')}>
+                <li className={currentPath === '/Style' ? 'selected' : ''} onClick={() => handleNavigation('/Style')}>
                     <div className="nav-item">
                         <FontAwesomeIcon icon={faPalette} size="2x" />
                     </div>
                     <span>Style</span>
                 </li>
-                <li onClick={() => handleNavigation('/Mystery')}>
+                <li className={currentPath === '/Mystery' ? 'selected' : ''} onClick={() => handleNavigation('/Mystery')}>
                     <div className="nav-item">
                         <div className="icon-container">
                             <FontAwesomeIcon icon={faMusic} size="2x" />
@@ -39,7 +40,7 @@ const App = () => {
                     </div>
                     <span>MystShare</span>
                 </li>
-                <li onClick={() => handleNavigation('/Privacy')}>
+                <li className={currentPath === '/Privacy' ? 'selected' : ''} onClick={() => handleNavigation('/Privacy')}>
                     <div className="nav-item">
                         <FontAwesomeIcon icon={faUserSecret} size="2x" />
                     </div>
