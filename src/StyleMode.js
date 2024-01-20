0542ee59217d3c3df1286af99292c67e1712001c
Luis Maiworm <luis.maiworm@outlook.de>
2024-01-20 16:05:38 +0100

    added EDIT functionality at StateMode, implemented Style Visuals

--- src/StyleMode.js
+++ src/StyleMode.js
@@ -1,26 +1,38 @@
 import React, {useState} from 'react';
-import Visualizer from "./Visualizer";
+
+import './StyleMode.css'; 
 
 const StyleMode = () => {
     const [selectedOption, setSelectedOption] = useState('');
+
     const handleSelectChange = (event) => {
         setSelectedOption(event.target.value);
     };
 
 
     return (
-        <div>
-            <label>Select an option for the visuals:</label>
-            <select value={selectedOption} onChange={handleSelectChange}>
-                <option value="">Select...</option>
-                <option value="Visuals1">Variante 1</option>
-                <option value="Visuals2">Variante 2</option>
-                <option value="Visuals3">Variante 3</option>
-            </select>
-            {selectedOption && (
-                <p>You selected: {selectedOption}</p>
-            )}
-            <Visualizer/>
+        <div className="style-container">
+            <div className="style-page">
+
+                <label>Select an option for the visuals:</label>
+                <select value={selectedOption} onChange={handleSelectChange}>
+                    <option value="">Select...</option>
+                    <option value="Visuals1">Variante 1</option>
+                    <option value="Visuals2">Variante 2</option>
+                    <option value="Visuals3">Variante 3</option>
+                </select>
+
+
+                {selectedOption && (
+                    <div>You selected: {selectedOption}</div>
+                )}
+                <div className="gif-container">
+                    <img src="sound_visual.webp" id="style-gif">
+
+                    </img>
+                </div>
+            </div>
+         
         </div>
     );
 };
